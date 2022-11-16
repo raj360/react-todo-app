65a84004e78fac2046c362478f90f8a6734174e4
raj360 <raymondkalumba360@gmail.com>
2022-11-17 01:46:20 +0300

    remove token from code

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -55,12 +55,6 @@ const testData = {
   user_id: 16,
   user_email: 'test@rapptrlabs.com',
   user_username: 'testuser',
-  user_is_active: 1,
-  user_profile_image: 'http://dev.rapptrlabs.com/Tests/images/taylor_avatar.png',
-  user_last_active_epoch: 1544680026,
-  user_creation_epoch: 1544713200,
-  user_is_new: 1,
-  user_token: '6dd4737a8b7ec61313ae5e900420d46815e1d13b2902be71b97a8fbf1f421a3e',
 };
 
 const Login = () => {
